04ba6cd925bb447d58e854b1ba9727ce00923805
pylemon <leeway1985@gmail.com>
2013-06-18 17:11:24 +0800

    add lambda mode and po model

--- personal/lw-init.el
+++ personal/lw-init.el
@@ -8,7 +8,84 @@
 (setq prelude-guru nil)
 (setq prelude-clean-whitespace-on-save nil)
 
-(prelude-ensure-module-deps '(smart-mode-line color-theme-sanityinc-tomorrow))
+(prelude-ensure-module-deps '(smart-mode-line 
+                              color-theme-sanityinc-tomorrow 
+                              multiple-cursors 
+                              edit-server
+                              web-mode))
+
+;;; enable colortheme
+(autoload 'color-theme-sanityinc-tomorrow-night "color-theme-sanityinc-tomorrow" " \(fn)" t nil)
+
+;; kill current buffer
+(defun yic-kill-current-buffer ()
+  (interactive)
+  (kill-buffer (current-buffer)))
+(global-set-key (kbd "M-`") 'yic-kill-current-buffer)
+
+
+;;; comment or uncomment region or line
+(defun comment-or-uncomment-region-or-line ()
+  "Like comment-or-uncomment-region, but if there's no mark \(that means no
+region\) apply comment-or-uncomment to the current line"
+  (interactive)
+  (if (not mark-active)
+      (comment-or-uncomment-region
+	(line-beginning-position) (line-end-position))
+      (if (< (point) (mark))
+	  (comment-or-uncomment-region (point) (mark))
+	(comment-or-uncomment-region (mark) (point)))))
+(global-set-key (kbd "M-;") 'comment-or-uncomment-region-or-line)
+
+
+;; highlight ipdb line in python-mode
+(defun annotate-pdb ()
+  (interactive)
+  (highlight-lines-matching-regexp "import ipdb")
+  (highlight-lines-matching-regexp "ipdb.set_trace()"))
+(add-hook 'python-mode-hook 'annotate-pdb)
+
+(defun python-add-breakpoint ()
+  (interactive)
+  (newline-and-indent)
+  (insert "import ipdb; ipdb.set_trace()"))
+(global-set-key (kbd "<f12>") 'python-add-breakpoint)
+
+
+;;; po mode for editing po files.
+(require 'po-mode)
+(add-to-list 'auto-mode-alist '("\\.po$" . po-mode))
+
+
+;;; enable linum-mode global
+(global-linum-mode 1)
+(scroll-bar-mode nil)
+(set-face-background hl-line-face "gray10")
+(setq ido-enable-flex-matching t)
+(setq ido-auto-merge-work-directories-length -1)
+
+
+;; bookmark location
+(setq bookmark-default-file "~/Dropbox/software_configs/bookmarks" bookmark-save-flag 1)
+
+
+;;; keybindings
+(global-set-key (kbd "C-c r") 'revert-buffer)
+(global-set-key (kbd "C-c q") 'join-line)
+(global-set-key (kbd "C-x 2") 'split-window-horizontally)
+(global-set-key (kbd "C-x 3") 'split-window-vertically)
+(global-set-key (kbd "C-x C-x") 'ido-switch-buffer)
+(global-set-key (kbd "C-\\") nil)
+(global-set-key (kbd "C-<return>") 'set-rectangular-region-anchor)
+(global-set-key (kbd "C-M-m") 'mc/mark-more-like-this-extended)
+
+
+;;; lambda shows in one char
+(require 'lambda-mode)
+(setq lambda-symbol (string (make-char 'greek-iso8859-7 107)))
+(add-hook 'python-mode-hook 'lambda-mode 1)
+(add-hook 'emacs-lisp-mode-hook 'lambda-mode 1)
+
 
 (require 'smart-mode-line)
 (add-hook 'after-init-hook 'sml/setup)
